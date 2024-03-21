d2dfb7071a5b85c3f1531220db6ef2d12d8390bf
jean <jean_0720@hotmail.com>
2024-03-20 22:43:36 -0500

    Creación modulo autenticación con JWT

---
+++ config/database.php
@@ -0,0 +1,24 @@
+<?php
+
+class DatabaseService{
+
+    private $db_host = "localhost";
+    private $db_name = "testeoapi_db";
+    private $db_user = "root";
+    private $db_password = "";
+    private $connection;
+
+    public function getConnection(){
+
+        $this->connection = null;
+
+        try{
+            $this->connection = new PDO("mysql:host=" . $this->db_host . ";dbname=" . $this->db_name, $this->db_user, $this->db_password);
+        }catch(PDOException $exception){
+            echo "Connection failed: " . $exception->getMessage();
+        }
+
+        return $this->connection;
+    }
+}
+?>
